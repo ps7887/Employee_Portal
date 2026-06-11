0399d07674b409ad494a2463135abc4f2fca9cff
Pragati <pragatisuryawanshi44@gmail.com>
2026-06-11 13:05:07 +0530

    create new mathod or controller

---
+++ Employee_Portal/Controllers/Employee1Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Portal.Controllers
+{
+    public class Employee1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
